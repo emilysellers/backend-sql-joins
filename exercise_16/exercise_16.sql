c307fc6b42fe85e08b71178aa4219edd2d734f32
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-08 15:44:43 -0800

    test 16 passing

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,14 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
-    
+
+SELECT film.title, film.description, film.release_year,
+COALESCE(
+  json_agg(to_jsonb(actor))
+  FILTER (WHERE actor.actor_id is NOT NULL), '[]') AS actors
+FROM film
+LEFT JOIN film_actor
+ON film.film_id = film_actor.film_id
+LEFT JOIN actor
+ON film_actor.actor_id = actor.actor_id
+WHERE film.title = 'Airplane Sierra'
+GROUP BY film.film_id
